b00902d2f7a2ce142c1a5b91f5c9b9756ff75132
Marwa Werfelli <marwa.werfelli@polytechnicien.tn>
2024-11-01 00:08:15 +0100

    add menu and navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,7 @@
+// import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +116,36 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = async () => {
+  const user = { publicMetadata: { role: "student" } };
+  // await currentUser();
+  const role = user?.publicMetadata.role as string;
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((item) => (
+        <div className="flex flex-col gap-2" key={item.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {item.title}
+          </span>
+          {item.items.map((list) => {
+            if (list.visible.includes(role)) {
+              return (
+                <Link
+                  href={list.href}
+                  key={list.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+                >
+                  <Image src={list.icon} alt="" width={20} height={20} />
+                  <span className="hidden lg:block">{list.label}</span>
+                </Link>
+              );
+            }
+          })}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
